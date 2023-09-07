d023e399bcd0cc24eca2fc76febe8946353622dd
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-09-07 03:01:37 +0200

    v6.4

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+const header = document.querySelector('header');
+var logo = document.getElementById("logo");
+
+window.addEventListener('scroll', () => {
+    if (window.innerWidth > 1000) {
+        if (window.scrollY > 100) {
+            header.style.height = '70px';
+            logo.style.height = '70px';
+        } else {
+            header.style.height = '120px';
+            logo.style.height = '110px';
+        }
+    }
+});
+
 function toggleNav() {
     if (window.innerWidth < 1000) {
         var header = document.getElementById("header");
@@ -5,14 +20,20 @@ function toggleNav() {
         var menuIcon = document.getElementById("button-menu");
         if (navLinks.style.display === "flex") {
             header.classList.remove("open-header");
+            header.style.height = '80px';
+            logo.style.height = '60px';
             navLinks.style.display = "none";
             menuIcon.classList.remove("icon-cross");
             menuIcon.classList.add("icon-menu");
         } else {
             header.classList.add("open-header");
+            header.style.height = '100%';
+            logo.style.height = '100px';
             navLinks.style.display = "flex";
             menuIcon.classList.remove("icon-menu");
             menuIcon.classList.add("icon-cross");
+            menuIcon.style.paddingRight = '20px';
+
         }
     }
 }
