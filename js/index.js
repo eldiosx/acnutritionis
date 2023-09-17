9accb380b023a14d2815018d77f022ca862573e8
“[eldiosx]” <[danieldelgadojimenez@protonmail.com]>
2023-09-17 22:30:30 +0200

    minorFixs

--- js/index.js
+++ js/index.js
@@ -81,52 +81,66 @@ document.getElementById("send").addEventListener("click", function () {
 });
 
 // Slider
-
 const background4 = document.querySelector('.background4');
 const prevBtn = document.querySelector('.icon-navigate_before');
 const nextBtn = document.querySelector('.icon-navigate_next');
 const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 let intervalId;
+
 function showSlide(index) {
     for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove('active');
-
     }
 
     slides[index].classList.add('active');
-
     currentIndex = index;
 }
+
 function showNextSlide() {
     background4.classList.add("background4-2");
     let newIndex = currentIndex + 1;
     if (newIndex >= slides.length) {
         newIndex = 0;
         background4.classList.remove("background4-2");
-
     }
     showSlide(newIndex);
 }
+
+function showPrevSlide() {
+    background4.classList.add("background4-2");
+    let newIndex = currentIndex - 1;
+    if (newIndex < 0) {
+        newIndex = slides.length - 1;
+        background4.classList.remove("background4-2");
+    }
+    showSlide(newIndex);
+}
+
 function startAutoSlide() {
     intervalId = setInterval(showNextSlide, 10000);
-
 }
+
 function stopAutoSlide() {
     clearInterval(intervalId);
 }
+
 prevBtn.addEventListener('click', function () {
     stopAutoSlide();
-    showNextSlide();
+    showPrevSlide();
     startAutoSlide();
 });
+
 nextBtn.addEventListener('click', function () {
     stopAutoSlide();
     showNextSlide();
     startAutoSlide();
 });
+
 showSlide(currentIndex);
 startAutoSlide();
+
+
 //Select pack
 const guide = document.getElementById("guidepack");
 const select = document.getElementById("selectplan");
