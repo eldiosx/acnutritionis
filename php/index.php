d7b1999ed30983f346f0a1aa0e1e68c7cf50f3d2
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-09-22 11:41:13 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,22 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
     $name = $_POST["name"];
     $phone = $_POST["telefono"];
     $email = $_POST["email"];
     $message = $_POST["info"];
-
-    $receiver = "acnutritionis@protonmail.com";
-    $main = "Consulta de $name";
-
-    $content = "Nombre: $name\n";
-    $content .= "Teléfono: $phone\n";
-    $content .= "Email: $email\n";
-    $content .= "Mensaje:\n$message\n";
-
-    $header = "From: $email\r\n";
-    $header .= "Reply-To: $email\r\n";
-
-    mail($receiver, $main, $content, $header);
-
-    header("Location: ./index.html");
+    
+    // Create email message
+    $to = "danieldelgadojimenez@protonmail.com";
+    $subject = "Contact Form AC Nutritionis";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $message_body = "Name: $name\n";
+    $message_body .= "Phone: $phone\n";
+    $message_body .= "Email: $email\n\n";
+    $message_body .= "Message:\n$message";
+    
+    // Send email
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Sorry, there was an error sending your message.";
+    }
+}
 ?>
