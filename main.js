b14f38f985488f803e7cd101ddb4f5edc6542c86
eldiosx <Linux>
2024-02-15 23:52:36 +0100

    purgeJS

--- main.js
+++ main.js
@@ -7,15 +7,9 @@ function toggleNav() {
   var menuIcon = document.getElementById("button-menu");
   var menuIcon2 = document.getElementById("button-menu2");
 
-  if (isNavOpen) {
-    header.classList.remove("open-header");
-    menuIcon.style.display = "flex";
-    menuIcon2.style.display = "none";
-  } else {
-    header.classList.add("open-header");
-    menuIcon2.style.display = "flex";
-    menuIcon.style.display = "none";
-  }
+  header.classList.toggle("open-header");
+  menuIcon.style.display = isNavOpen ? "flex" : "none";
+  menuIcon2.style.display = isNavOpen ? "none" : "flex";
   isNavOpen = !isNavOpen;
 }
 
@@ -23,64 +17,45 @@ function toggleNav() {
 const prevBtn = document.querySelector(".navigate_left");
 const nextBtn = document.querySelector(".navigate_right");
 const slides = document.querySelectorAll(".slide");
-let currentIndex = 0;
-let intervalId;
+let currentIndex = 1;
 
-showSlideNext(currentIndex);
+toggleSlide();
 
-function showSlideNext(index) {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("activeLeft");
-    slides[i].classList.remove("activeRight");
-  }
+function toggleSlide(direction) {
+  let newIndex = currentIndex + (direction === "next" ? 1 : -1);
+  newIndex = newIndex < 0 ? slides.length - 1 : newIndex % slides.length;
 
-  slides[index].classList.add("activeRight");
-  currentIndex = index;
+  slides.forEach((slide, index) => {
+    slide.classList.remove("activeLeft", "activeRight");
+    if (index === newIndex) {
+      slide.classList.add(direction === "next" ? "activeRight" : "activeLeft");
+    }
+  });
+
+  currentIndex = newIndex;
 }
 
-function showSlidePrev(index) {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("activeRight");
-    slides[i].classList.remove("activeLeft");
-  }
-
-  slides[index].classList.add("activeLeft");
-  currentIndex = index;
-}
-
-function showNextSlide() {
-  let newIndex = currentIndex + 1;
-  if (newIndex >= slides.length) {
-    newIndex = 0;
-  }
-  showSlideNext(newIndex);
-}
-
-function showPrevSlide() {
-  let newIndex = currentIndex - 1;
-  if (newIndex < 0) {
-    newIndex = slides.length - 1;
-  }
-  showSlidePrev(newIndex);
-}
-
-prevBtn.addEventListener("click", function () {
-  showPrevSlide();
-});
-
-nextBtn.addEventListener("click", function () {
-  showNextSlide();
-});
+prevBtn.addEventListener("click", () => toggleSlide("prev"));
+nextBtn.addEventListener("click", () => toggleSlide("next"));
 
 //Copy
+function copyToClipboard(elementId) {
+  const text = document.getElementById(elementId).textContent;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("Texto copiado con éxito");
+    })
+    .catch((err) => {
+      console.error("Error al copiar texto: ", err);
+    });
+}
+
 function copynumber() {
-  var texto = document.getElementById("number");
-  var textArea = document.createElement("textarea");
-  textArea.value = texto.textContent;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
+  copyToClipboard("number");
+}
+function copyemail() {
+  copyToClipboard("email");
 }
 
 //Loader
@@ -89,111 +64,24 @@ window.addEventListener("load", function () {
   loadingContainer.style.display = "none";
 });
 
-// Translations
-const translations = {
-  "": "",
-  Inicio: "Home",
-  Nutrición: "Nutrition",
-  "Sobre mí": "About me",
-  Servicios: "Services",
-  Consultas: "Appointments",
-  Contacto: "Contact",
-  "🇪🇦": "🇬🇧",
-  "Nutrición para la recuperación": "Nutrition for Recovery",
-  "¡Hola!, soy Alejandro Campos dietista-nutricionista y te ayudaré a dejar de lado las dietas restrictivas, a que aprendas a comer sin culpa y a que seas capaz de disfrutar del placer de la alimentación.":
-    "Hello! I'm Alejandro Campos, a dietitian-nutritionist, and I'll help you move away from restrictive diets, learn to eat without guilt, and embrace the pleasure of food.",
-  "¡Quiero una cita!": "I want a date!",
-  "Alimentación consciente": "Mindful eating",
-  "Consigamos juntos dejar de lado a la cultura de la dieta, permitiéndote cultivar aspectos como la autocompasión, el placer por la comida y a reconectar con tus señales de hambre y saciedad.":
-    "Let's work together to set aside diet culture, allowing you to cultivate aspects such as self-compassion, enjoyment of food, and reconnecting with your hunger and fullness cues.",
-  "¡Crea tu plan de acción!": "Create your action plan!",
-  "Consultas Online": "Online consultations",
-  "Si quieres conocer más sobre cómo establecer y mantener una relación sana con la comida desde la teoría hasta la práctica en directo conmigo y de forma personalizada, esta opción es para ti.":
-    "If you want to learn more about how to establish and maintain a healthy relationship with food, from theory to practical application, live and personalized with me, then this option is for you.",
-  "Mi pasión siempre ha sido ayudar a las personas a mejorar su calidad de vida mediante una alimentación flexible y equilibrada, evitando la obsesión por la comida y las dietas restrictivas.":
-    "My passion has always been to help people improve their quality of life through a flexible and balanced diet, while avoiding obsession with food and restrictive diets.",
-  "Siempre me ha apasionado la nutrición clínica, por ello me he especializado en el área de los TCA y obesidad.":
-    "I have always been passionate about clinical nutrition; that's why I have specialized in the field of eating disorders and obesity.",
-  "Mi método de trabajo se basa en aplicar las bases de una alimentación flexible, saludable, consciente y personalizada en función tus necesidades, siendo capaz de adaptarme a cada situación.":
-    "My working method is based on applying the principles of flexible, healthy, mindful, and personalized nutrition according to your needs, and I am capable of adapting to each situation.",
-  "Asesoramiento Nutricional": "Nutritional Counseling",
-  "Trastornos de la conducta alimentaria": "Eating Disorders",
-  "Reeducación alimentaria": "Nutritional reeducation",
-  "Alimentación en pacientes con patologías digestivas (SII, SIBO etc.)":
-    "Nutrition for patients with digestive disorders (such as IBS, SIBO, etc.)",
-  "Alergias e intolerancias": "Allergies and Intolerances",
-  "Alimentación vegana y vegetariana": "Vegan and vegetarian diets",
-  "Alimentación para pacientes con obesidad, DM2 etc.":
-    "Nutrition for obese patients, DM2, etc.",
-  "Coaching Nutricional": "Nutritional Coaching",
-  "Te ayudaré a mejorar tu alimentación y salud, trabajando diferentes conceptos desde la autocompasión, la motivación y empatía":
-    "I will help you improve your diet and health by addressing various concepts such as self-compassion, motivation, and empathy",
-  "Juntos identificaremos tus objetivos y serás capaz de vencer los obstáculos durante el proceso":
-    "Together, we will identify your goals, and you will be able to overcome any obstacles that arise during the process",
-  "Aprenderás a usar las herramientas necesarias para conseguir tus objetivos":
-    "You will learn how to use the necessary tools to achieve your goals",
-  "Mejorarás tu calidad de vida": "You will improve your quality of life",
-  "Disfrutarás del placer de la alimentación":
-    "You will enjoy the pleasure of eating",
-  "Consultas Personalizadas": "Custom Queries",
-  "¿Cómo trabajo?": "How do I work?",
-  "1ª y 2ª Consulta": "1ª y 2ª Meeting",
-  "Se realizarán 1 vez por semana o cada dos semanas en función de tus necesidades. En cada seguimiento se realizará:":
-    "They will be conducted once a week or every two weeks, depending on your needs. Each follow-up will include:",
-  "Actualización de tu estado": "Update your status",
-  "Análisis de logros y dificultades":
-    "Analysis of achievements and difficulties",
-  "Revisión de registros y actividades": "Review of records and activities",
-  "Modificación de tu planificación": "Modification of your dietary plan",
-  "Duración: 30 minutos": "Duration: 30 minutes",
-  Duración: "Duration",
-  Seguimientos: "Follow-ups",
-  "¿Qué incluyen?": "What do they include?",
-  "Elaboración del historial clínico": "Patient's medical history",
-  "Estudio nutricional inicial": "Initial nutritional assessment",
-  "Establecimiento de objetivos": "Setting goals",
-  "Análisis de tu historia de dietas": "Analysis of your diet history",
-  "Tratamiento nutricional personalizado": "Personalized nutritional treatment",
-  "Entrega y explicación de tu plan":
-    "Providing and explaining your dietary plan",
-  "Recomendaciones adaptadas a ti": "Customized recommendations",
-  "Ideas de recetas nutritivas": "Nutritious recipe ideas",
-  "eBook guía de nutrición inicial": "Initial nutrition guide eBook",
-  "Duración: La 1ª consulta dura al rededor de 1 hora, para conocer tus necesidades. El resto de consultas duran 40 minutos aproximadamente.":
-    "Duration: The 1st consultation lasts about 1 hour to understand your needs. The remaining consultations last approximately 40 minutes.",
-  "Escoge un plan para saber mas información y su precio.":
-    "Choose a plan to access more information and its price.",
-  "Selecciona tu plan": "Choose your plan",
-  "PLAN GRATUITO": "FREE PLAN",
-  "PLAN DE INICIO": "INITIAL PLAN",
-  CONSULTA: "APPOINTMENTS",
-  SEGUIMIENTOS: "FOLLOW-UPS",
-  "¿Aún tienes dudas sobre empezar tu plan? Conóceme eligiendo este pack y concertaremos una sesión de unos 20-30 min para que me cuentes tu situación de forma general y conozcas mi método de trabajo sin ningún compromiso.":
-    "Do you still have doubts about starting your plan? Get to know me by selecting this package, and we will schedule a 20-30 minute session for you to tell me about your situation in a general way and learn about my working method without any commitment.",
-  "Incluye la 1ª y 2ª consulta por videollamada conmigo, el estudio y valoración de tus necesidades, herramientas nutricionales y prácticas para logar tus objetivos así como la entrega de tu tratamiento nutricional personalizado.":
-    "Includes the 1st and 2nd video call consultations with me, the assessment and evaluation of your needs, nutritional tools, and practices to achieve your goals, along with the delivery of your personalized nutritional treatment.",
-  "Contrata una consulta individual para empezar con tu tratamiento personalizado. Te proporcionaré herramientas prácticas y teóricas que te ayudarán a lograr tus objetivos y reconectar con tus señales biológicas.":
-    "Hire an individual consultation to start with your personalized treatment. I will provide you with practical and theoretical tools that will help you achieve your goals and reconnect with your biological signals.",
-  "Los seguimientos se realizan a las 3-4 semanas del tratamiento, puede realizarse uno cada semana o cada dos semanas en función de cada paciente con el objetivo de resolver dificultades, dudas, educación nutricional y la modificación del plan dietético.":
-    "The follow-ups are conducted 3-4 weeks after the treatment, and they can be scheduled weekly or every two weeks, depending on each patient, with the goal of addressing difficulties, answering questions, providing nutritional education, and adjusting the diet plan.",
-  Precio: "Price",
-  "Solicita ya tu consulta online": "Request your online consultation now",
-  Nombre: "Name",
-  Descargar: "Download",
-  Bloqueado: "Lock",
-  "1ª Consulta": "1st Meeting",
-  "Para cualquier otro tipo de consultas, dudas o propuestas, ¡contáctame ahora!":
-    "For any other inquiries, doubts, or proposals, contact me now!",
-  "(Recibirás siempre una respuesta en un plazo de 24-48 H días hábiles)":
-    "(You will always receive a response within 24-48 business hours.)",
-  Teléfono: "Phone",
-  Mensaje: "Message",
-  "Todos los derechos reservados ©2023 - GPL 3.0":
-    "All rights reserved ©2023 - GPL 3.0",
-};
-const elementsToTranslate = document.querySelectorAll("[data-translate]");
-let isTranslated = getCookie("isTranslated") === "es";
-function setCookie(name, value, days) {
+//Translate
+let translations = JSON.parse(localStorage.getItem("translations")) || {};
+if (Object.keys(translations).length === 0) {
+  fetchTranslations();
+}
+
+function fetchTranslations() {
+  fetch("i18n.json")
+    .then((response) => response.json())
+    .then((data) => {
+      translations = data;
+      localStorage.setItem("translations", JSON.stringify(data));
+      applyTranslation();
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function setCookie(name, value, days, sameSite = "Strict") {
   const expires = new Date(
     Date.now() + days * 24 * 60 * 60 * 1000
   ).toUTCString();
@@ -209,37 +97,24 @@ function getCookie(name) {
   return cookie ? decodeURIComponent(cookie[1]) : null;
 }
 
+const elementsToTranslate = document.querySelectorAll("[data-translate]");
+let isTranslated = getCookie("isTranslated") === "es";
+
 function applyTranslation() {
   elementsToTranslate.forEach((element) => {
-    const originalText = element.getAttribute("data-translate");
-    const translatedText = translations[originalText];
-    if (isTranslated && translatedText) {
-      element.textContent = translatedText;
-    } else {
-      element.textContent = originalText;
-    }
+    const key = element.getAttribute("data-translate");
+    element.textContent =
+      isTranslated && translations[key] ? translations[key] : key;
   });
 }
+
 function toggleLang() {
   isTranslated = !isTranslated;
-  setCookie("isTranslated", isTranslated ? "es" : "en", 30);
+  setCookie("isTranslated", isTranslated ? "es" : "en", 30, "Strict");
   applyTranslation();
-  location.reload();
 }
+
 applyTranslation();
-const translateButton = document.querySelector(".translateButton");
-translateButton.addEventListener("click", toggleTranslation);
-const translateButtonUserMenu = document.querySelector(
-  ".menuUsercontent .translateButton"
-);
-translateButtonUserMenu.addEventListener("click", toggleTranslation);
-let language = isTranslated ? "es" : "en";
-function copyemail() {
-  var texto = document.getElementById("email");
-  var textArea = document.createElement("textarea");
-  textArea.value = texto.textContent;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
-}
+document.querySelectorAll(".translateButton").forEach((button) => {
+  button.addEventListener("click", toggleLang);
+});
